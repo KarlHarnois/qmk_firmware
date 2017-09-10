7bf54139153aa165ac9dd95bf6601780cc850b8e
KarlHarnois <karl@goDynamo.com>
2017-09-10 18:30:51 -0400

    Add karl's planck keymap

---
+++ keyboards/planck/keymaps/karl/keymap.c
@@ -0,0 +1,108 @@
+// docker run -e keymap=karl -e subproject=rev4 -e keyboard=planck --rm -v $('pwd'):/qmk:rw edasque/qmk_firmware
+
+#include "planck.h"
+#include "action_layer.h"
+
+extern keymap_config_t keymap_config;
+
+enum planck_layers {
+  _QWERTY,
+  _LOWER,
+  _RAISE,
+};
+
+enum planck_keycodes {
+  QWERTY = SAFE_RANGE,
+  LOWER,
+  RAISE,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/* Qwerty
+ * ,-----------------------------------------------------------------------------------.
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Shift|
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Esc  |Raise | Alt  | GUI  |Lower |    Space    |Raise | GUI  | Alt  |Lower | Ctrl |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_QWERTY] = {
+  {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
+  {KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
+  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT},
+  {KC_ESC,  RAISE,   KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RGUI, KC_RALT, LOWER,   KC_RCTL}
+},
+
+/* Lower
+ * ,-----------------------------------------------------------------------------------.
+ * |   ~  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |  -   | 
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | ctrl |      |      |      |      |      | left | down |  up  |right |   |  |  \   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |shift |      |      |      |      |      |      |ISO ~ |ISO | | Home | End  |shift |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | esc  |      | alt  | gui  |      |    Bksp     |      | gui  | alt  |      |ctrl  |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_LOWER] = {
+  {KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,       KC_8,       KC_9,     KC_0,    KC_BSPC},
+  {KC_LCTL, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN,    KC_UP,      KC_RIGHT, KC_PIPE, KC_BSLASH},
+  {KC_LSFT, _______, _______, _______, _______, _______, _______, S(KC_NUHS), S(KC_NUBS), KC_HOME,  KC_END,  KC_RSFT},
+  {KC_ESC,  _______, KC_LALT, KC_LGUI, _______, KC_BSPC, KC_BSPC, _______,    KC_RGUI,    KC_RALT,  _______, KC_RCTL}
+},
+
+/* Raise
+ * ,-----------------------------------------------------------------------------------.
+ * |   `  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |  -   |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | ctrl |      |      |      |      |      |      | Pg up|Pg dn |   [  |   ]  |  =   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |shift |      |      |      |      |      |      |      |      |   {  |   }  |shift |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | esc  |      | alt  | gui  |      |   enter     |      | gui  | alt  |      | ctrl |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_RAISE] = {
+  {KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR,   KC_LPRN, KC_RPRN, KC_MINUS},
+  {KC_LCTL, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_PGDOWN, KC_LBRC, KC_RBRC, KC_EQUAL},
+  {KC_LSFT, _______, _______, _______, _______, _______, _______, _______, _______,   KC_LCBR, KC_RCBR, KC_RSFT},
+  {KC_ESC,  _______, KC_LALT, KC_LGUI, _______, KC_ENT,  KC_ENT,  _______, KC_RGUI,   KC_RALT, _______, KC_RCTL}
+},
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case QWERTY:
+      if (record->event.pressed) {
+        print("mode just switched to qwerty and this is a huge string\n");
+        set_single_persistent_default_layer(_QWERTY);
+      }
+      return false;
+      break;
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _RAISE);
+      } else {
+        layer_off(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _RAISE);
+      }
+      return false;
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _RAISE);
+      } else {
+        layer_off(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _RAISE);
+      }
+      return false;
+      break;
+  }
+  return true;
+}
